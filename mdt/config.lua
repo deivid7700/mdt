2cb57231dfebf540abb282f371ed9c45233f3a2e
deivid7700 <90959803+deivid7700@users.noreply.github.com>
2022-02-23 18:56:20 +0100

    Add files via upload

---
+++ mdt/config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.Notify = 'esx' -- mythic (new) / esx
+Config.Webhook = 'https://discord.com/api/webhooks/945380154912411729/Wf3rfvHY9-Zk6YxUw5NlqX4Ja5Cw77qM9HOjekQ6QOAZ1kJ5tYyfu_aT05rnoeYHsG5G' -- Place your discord webhook here for using screenshot feature 
+Config.KeyMapping = 'delete' -- Control action for open the MDT
